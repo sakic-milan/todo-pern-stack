c136ba66b315724afbd099df0af2f9bc5d291854
sakicmilan <sakic.milan@hotmail.com>
2021-10-30 23:36:20 +0200

    db connection string fix

--- db.js
+++ db.js
@@ -3,9 +3,7 @@ require("dotenv").config();
 
 const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
-const proConfig = {
-  connectionString: process.env.DATABASE_URL,
-};
+const proConfig = process.env.DATABASE_URL;
 
 const pool = new Pool({
   connectionString:
