f4030faa71113003dfb46e021187d061b24b028b
sakicmilan <sakic.milan@hotmail.com>
2021-10-31 18:49:58 +0100

    test

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 
 //process.env.PORT
 //process.env.NODE_ENV => production or undefined
-
+await pool.connect();
 //middleware
 app.use(cors());
 app.use(express.json());
