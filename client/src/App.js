6eefc064d8a2707176b5582ca0700395f1d1add3
sakicmilan <sakic.milan@hotmail.com>
2021-10-31 18:32:31 +0100

    heroku test

--- client/src/App.js
+++ client/src/App.js
@@ -93,12 +93,10 @@ function App() {
     setTodos(todos);
   };
 
-  console.log(
-    `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
-  );
+  console.log(process.env.PROD_DATABASE_URL);
 
   useEffect(() => {
-    // fetchTodos();
+    fetchTodos();
   }, []);
 
   return (
