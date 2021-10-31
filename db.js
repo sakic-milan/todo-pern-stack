8e019d39e769e0e35c46e93605e14f2ac8c07315
sakicmilan <sakic.milan@hotmail.com>
2021-10-31 18:36:18 +0100

    heroku test

--- db.js
+++ db.js
@@ -3,7 +3,8 @@ require("dotenv").config();
 
 const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
-const proConfig = process.env.PROD_DATABASE_URL;
+const proConfig =
+  "postgres://dhelyqvbgrsymh:55dd3e400a722de6d0a5abccfdca2baf0a79f1b8df8edc7e91ee599ee6c5d03f@ec2-3-233-55-123.compute-1.amazonaws.com:5432/dcgkra10ejqqtq";
 
 const pool = new Pool({
   connectionString:
